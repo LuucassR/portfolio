32210c9c19943be693e97248dd027c8734906049
Cookie <lucassrossi12@gmail.com>
2026-01-10 22:53:44 -0300

    change vite.congif.ts base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/console-portfolio/',
+  base: '/portfolio/',
   plugins: [react(), tailwindcss()],
 });
